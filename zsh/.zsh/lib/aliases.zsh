439a693f3ac5c8f0c7dd3510b4b307fde4e67201
karming <karming.chin@mail.mcgill.ca>
2022-11-16 22:30:42 -0500

    more aliases

--- zsh/.zsh/lib/aliases.zsh
+++ zsh/.zsh/lib/aliases.zsh
@@ -1,3 +1,14 @@
+# global aliases
+_C() {
+    # USAGE: _C COLUMN
+    # Get column COLUMN from space seperated output
+    typeset column=$1
+    awk "{ print \$$column }"
+}
+
+alias -g G="| grep"
+alias -g L="| less"
+
 # terminal stuff
 alias sclear="clear && printf '\e[3J'"
 
