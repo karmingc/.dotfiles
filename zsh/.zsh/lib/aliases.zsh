cde4ff9a050124bcdfbeba0c4da39fa4ecef85ee
karming <karming.chin@mail.mcgill.ca>
2022-11-30 21:25:15 -0500

    aliases

--- zsh/.zsh/lib/aliases.zsh
+++ zsh/.zsh/lib/aliases.zsh
@@ -8,9 +8,14 @@ _C() {
 
 alias -g G="| grep"
 alias -g L="| less"
+alias -g NV="nvim"
+alias -g aliases="~/.dotfiles/zsh/.zsh/lib/aliases.zsh"
 
 # terminal stuff
 alias sclear="clear && printf '\e[3J'"
+
+# Personal laptop
+alias cdhrepos="cd ~/documents/engineering/repos"
 
 # kubectl
 alias k="kubectl"
