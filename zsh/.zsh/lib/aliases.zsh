49dba36127670797c24d9b3a373d8585a91e2020
Karming <karming.chin@mail.mcgill.ca>
2023-01-02 16:03:19 -0500

    feat: adding more aliases

--- zsh/.zsh/lib/aliases.zsh
+++ zsh/.zsh/lib/aliases.zsh
@@ -21,5 +21,32 @@ alias cdhrepos="cd ~/documents/engineering/repos"
 alias k="kubectl"
 alias kpo="k get po"
 
+# git
+alias g='git'
+alias gb='git branch'
+alias gco='git checkout'
+alias ga='git add'
+alias gs='git status --short'
+alias gl='git log --decorate'
+alias gl1p='git log --decorate -1 -p'
+alias gls='git log --decorate --stat'
+alias gci='git commit -v'
+alias gpl='git pull'
+alias cdg='cd "$(git rev-parse --show-toplevel)"'
+alias gciah='git commit --amend -C HEAD'
+alias gr='git rebase'
+alias grc='git rebase --continue'
+alias gra='git rebase --abort'
+alias gd='git diff'
+alias gdc='git diff --cached'
+
 # helm
 alias h="helm"
+
+# pulumi
+#########
+alias pu=pulumi
+alias pl='pulumi stack ls'
+alias pss='pulumi stack select'
+alias pp='pulumi preview'
+alias ppd='pulumi preview --diff'
